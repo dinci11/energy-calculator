cb338e1f7e60aca99cd93c3a7603f3c703fd8e0d
Denes <deel01@betssongroup.com>
2024-02-09 20:20:41 +0100

    Initialize application

---
+++ EnergyCalculator/EnergyCalculator/Application.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace EnergyCalculator
+{
+    public class Application
+    {
+        private readonly ILogger<Application> _logger;
+
+        public Application(ILogger<Application> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task RunAsync()
+        {
+            _logger.LogInformation("Application started...");
+            await Task.Delay(1000);
+            _logger.LogInformation("Application shutting down...");
+        }
+    }
+}
---
+++ EnergyCalculator/EnergyCalculator/Program.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace EnergyCalculator
+{
+    public class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
+
+            var application = host.Services.GetService<Application>();
+
+            await application.RunAsync();
+        }
+
+        private static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            var host = Host.CreateDefaultBuilder(args);
+            ConfigureLogging(host);
+            ConfigureServices(host);
+            return host;
+        }
+
+        private static void ConfigureLogging(IHostBuilder host)
+        {
+            host.ConfigureLogging(logging =>
+            {
+                logging.AddConsole();
+            });
+        }
+
+        private static void ConfigureServices(IHostBuilder host)
+        {
+            host.ConfigureServices((context, services) =>
+            {
+                services.AddSingleton<Application>();
+            });
+        }
+    }
+}
